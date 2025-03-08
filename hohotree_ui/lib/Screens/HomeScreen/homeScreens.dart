2ce9aff123d9d4cd66f60534abe91a235ada388e
oafnghh <minhhoanglee11@gmail.com>
2025-03-08 12:06:38 +0700

    logic fe

--- hohotree_ui/lib/Screens/HomeScreen/homeScreens.dart
+++ hohotree_ui/lib/Screens/HomeScreen/homeScreens.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:hohotree/Screens/HomeScreen/widget/buildBtn.dart';
+import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -13,7 +15,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   File? _image;
-
+  String _result = "";
   Future<void> _pickImage() async {
     final picker = ImagePicker();
     final pickedFile = await picker.pickImage(source: ImageSource.camera);
@@ -22,7 +24,26 @@ class _HomeScreenState extends State<HomeScreen> {
       setState(() {
         _image = File(pickedFile.path);
       });
+      await _analyzeImage(_image!);
     }
+  }
+  // Xử lí khi gửi ảnh nha bành hoài 
+  Future<void> _analyzeImage(File imageFile) async {
+    var request = http.MultipartRequest(
+      'POST', 
+      Uri.parse('')
+    );
+    
+    request.files.add(
+      await http.MultipartFile.fromPath('file', imageFile.path),
+    );
+    var response = await request.send();
+    var responseData = await response.stream.bytesToString();
+    var jsonData = json.decode(responseData);
+
+    setState(() {
+      _result = jsonData['disease'];
+    });
   }
   @override
   Widget build(BuildContext context) {
@@ -55,6 +76,13 @@ class _HomeScreenState extends State<HomeScreen> {
               buildButton('Quét cây trồng',_pickImage),
               SizedBox(height: 10),
               buildButton('Những vấn đề chung', () {}),
+              SizedBox(height: 20),
+              _image != null ? Image.file(_image!, height: 200) : Container(),
+              SizedBox(height: 10),
+              Text(
+              _result.isNotEmpty ? _result : 'Kết quả sẽ hiển thị ở đây',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+              ),
             ],
           ),
         ],
