a7a9e7987027bce9d7154509407c290ffe2500bd
ThuongHoai081 <th316942@gmail.com>
2025-03-09 16:15:45 +0700

    structure be

--- hohotree_be/hohotree_be/plant_disease/apps.py
+++ hohotree_be/hohotree_be/plant_disease/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PlantDiseaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'plant_disease'
+    name = 'hohotree_be.plant_disease'
